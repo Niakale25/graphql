4592e049a158111da06d222763263861381d0cdd
niakale <diakitetenin99@gmail.com>
2025-07-17 16:47:32 +0000

    nouvelle ajout

--- src/main/java/com/example/graphqlserver/Author.java
+++ src/main/java/com/example/graphqlserver/Author.java
@@ -3,12 +3,17 @@ package com.example.graphqlserver;
 import java.util.Arrays;
 import java.util.List;
 
-public record Author(String id, String firstname, String lastname) {
+public record Author(String id, String firstName, String lastName) {
     private static List<Author> authors = Arrays.asList(
-       new Author("auter-1", "Niakalé", "Diakité"),
-       new Author("auther-2", "Gaoussou", "Diakité"),
-       new Author("auther-3", "Awa", "Sow")
+       new Author("author_1", "Niakalé", "Diakité"),
+       new Author("author_2", "Gaoussou", "Diakité"),
+       new Author("author_3", "Awa", "Sow")
 
     );
-
+    public static Author getById (String id){
+        return authors.stream()
+                .filter(author -> author.id().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
 }
--- src/main/java/com/example/graphqlserver/Book.java
+++ src/main/java/com/example/graphqlserver/Book.java
@@ -7,9 +7,9 @@ import java.util.stream.Stream;
 
 public record Book(String id, String name, int pageCount, String authorId) {
     private static List<Book> books = Arrays.asList(
-            new Book("Book_1", "name_1", 112, "Author_1"),
-            new Book("Book_2", "name_2", 113, "Author_2"),
-            new Book("Book_3", "name_3", 114, "Author_3")
+            new Book("Book_1", "name_1", 112, "author_1"),
+            new Book("Book_2", "name_2", 113, "author_2"),
+            new Book("Book_3", "name_3", 114, "author_3")
     );
     public static Book getById(String id){
         return books.stream()
